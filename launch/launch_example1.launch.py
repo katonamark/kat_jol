5bcb0029364bf84d4f3094545ac3b67182e071e0
katonamark <katona.mark003@gmail.com>
2025-03-18 17:09:36 +0100

    name changes

--- launch/launch_example1.launch.py
+++ launch/launch_example1.launch.py
@@ -25,7 +25,7 @@ def generate_launch_description():
             ]
         ),
         # Node(
-        #     package='ros2_cpp_template',
+        #     package='kat_jol_ros2_package',
         #     executable='simple_sub_node',
         #     output='screen',
         # ),
